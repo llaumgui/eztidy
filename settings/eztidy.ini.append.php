83de1e3c35f70c409414b41acd47abad3c073cb0
LLaumgui <LLaumgui@1fce53b6-e0f6-49e8-ae06-7676f4c95ac0>
2008-12-08 11:18:53 +0000

    initial import

---
+++ settings/eztidy.ini.append.php
@@ -0,0 +1,54 @@
+<?php /* #?ini charset="utf-8"?
+
+[Global]
+# Global charset past to Tidy
+Charset=utf8
+# Add <!-- Tidy - Begin --> & <!-- Tidy - End -->
+ShowTidyElement=disabled
+
+
+
+[TidyCache]
+ViewCache=enabled
+TemplateBlock=enabled
+
+# Tidy cache configuration
+# Look at http://tidy.sourceforge.net/docs/quickref.html
+Configuration[]
+# HTML, XHTML, XML Options Reference
+Configuration[anchor-as-name]=0
+Configuration[alt-text]=
+Configuration[hide-comments]=1
+Configuration[indent-cdata]=1
+Configuration[output-xhtml]=1
+Configuration[show-body-only]=1
+# Diagnostics Options Reference
+# Pretty Print Options Reference
+Configuration[indent]=1
+Configuration[indent-spaces]=2
+Configuration[wrap]=0
+# Character Encoding Options Reference
+# Miscellaneous Options Reference
+
+
+
+[Tidy]
+# Tidy template operator configuration
+# Look at http://tidy.sourceforge.net/docs/quickref.html
+Configuration[]
+# HTML, XHTML, XML Options Reference
+Configuration[anchor-as-name]=0
+Configuration[alt-text]=
+Configuration[hide-comments]=1
+Configuration[indent-cdata]=1
+Configuration[output-xhtml]=1
+Configuration[show-body-only]=1
+# Diagnostics Options Reference
+# Pretty Print Options Reference
+Configuration[indent]=1
+Configuration[indent-spaces]=2
+Configuration[wrap]=0
+# Character Encoding Options Reference
+# Miscellaneous Options Reference
+
+*/ ?>
