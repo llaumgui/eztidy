8594ab6318c4e28bc8529eddbcb6841de2832cd3
LLaumgui <LLaumgui@1fce53b6-e0f6-49e8-ae06-7676f4c95ac0>
2011-01-30 12:49:22 +0000

    Updat copyright

--- classes/eztidy.php
+++ classes/eztidy.php
@@ -2,30 +2,31 @@
 //
 // Definition of eZTidy class
 //
-// Created on: <28-nov-2008 12:14:34 bf>
+// Created on: <28 nov. 2008 10:50:20 llaumgui>
 //
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZTidy
-// SOFTWARE RELEASE: 0.9
-// BUILD VERSION:
-// COPYRIGHT NOTICE: Copyright (c) 2008 Guillaume Kulakowski and contributors
+// SOFTWARE RELEASE: 1.0.0
+// COPYRIGHT NOTICE: Copyright (C) 2008-2011 Guillaume Kulakowski
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 2.0 of the GNU General
+// Public License as published by the Free Software Foundation.
 //
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
+// You should have received a copy of version 2.0 of the GNU General
+// Public License along with this program; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
 //
 //
-
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
 
 /*! \file eztidy.php
 */
@@ -42,10 +43,10 @@ class eZTidy
     public $isTidyfied = false;
 
 
-    /*!
-     Constructor
-
-     \param $conf_section string
+    /**
+     * Constructor
+     *
+     * @param $conf_section string
      */
     function __construct( $conf_section )
     {
@@ -59,11 +60,12 @@ class eZTidy
     }
 
 
-    /*!
-     Get a new instance of eZTidy
 
-     \param $conf_section string
-     \return object eZTidy
+    /**
+     * Get a new instance of eZTidy
+     *
+     * @param $conf_section string
+     * @return object eZTidy
      */
     static function instance( $conf_section = "Tidy" )
     {
@@ -83,8 +85,8 @@ class eZTidy
 
 
 
-    /*!
-     Show tidy warning
+    /**
+     * Show tidy warning
      */
     private function reportWarning()
     {
@@ -95,11 +97,10 @@ class eZTidy
 
 
 
-    /*!
-     Tidyfication des strings
-
-     \param $str string
-     \return string
+    /**
+     * Tidyfication of the strings
+	 * @param $str string
+     * @return string
      */
     public function tidyCleaner ( $str )
     {
--- classes/eztidyfilter.php
+++ classes/eztidyfilter.php
@@ -2,28 +2,30 @@
 //
 // Definition of eZTidyFilter class
 //
-// Created on: <28-nov-2008 12:14:34 bf>
+// Created on: <28 nov. 2008 10:50:20 llaumgui>
 //
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZTidy
-// SOFTWARE RELEASE: 0.9
-// BUILD VERSION:
-// COPYRIGHT NOTICE: Copyright (c) 2008 Guillaume Kulakowski and contributors
+// SOFTWARE RELEASE: 1.0.0
+// COPYRIGHT NOTICE: Copyright (C) 2008-2011 Guillaume Kulakowski
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 2.0 of the GNU General
+// Public License as published by the Free Software Foundation.
 //
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
+// You should have received a copy of version 2.0 of the GNU General
+// Public License along with this program; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
 //
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
 
